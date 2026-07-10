8b1cd3526ebfcdf324d6ed0ac53dd2f9043e2892
Mariyan Donchev <pi4aaa@abv.bg>
2026-07-10 18:44:52 +0300

    Create AuthContext.jsx

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,49 @@
+import { createContext, useContext, useState } from "react";
+import usePersistedState from "../hooks/usePersistedState";
+
+
+export const AuthContext = createContext({
+    fullName: '',
+    userId: '',
+    email: '',
+    accessToken: '',
+    isAuthenticated: false,
+    changeAutState: (authState = {}) => null,
+    logout: () => null,
+});
+
+export function AuthContextProvider(props) {
+
+    // const [authState, setAuthState] = useState({});
+    const [authState, setAuthState] = usePersistedState('auth', {});
+
+    const changeAutState = (state) => {
+        setAuthState(state);
+    };
+
+    const logout = () => {
+        setAuthState(null)
+    }
+
+    const contextData = {
+        fullName: authState?.fullName,
+        userId: authState?._id,
+        email: authState?.email,
+        accessToken: authState?.accessToken,
+        isAuthenticated: !!authState?.email,
+        changeAutState,
+        logout,
+    };
+
+    return (
+        <AuthContext.Provider value={contextData}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
+export function useAuthContext() {
+    const authData = useContext(AuthContext);
+
+    return authData;
+}

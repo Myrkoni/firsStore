6fead780ca8a040672aa1937ceb323e2bcc5de06
Mariyan Donchev <pi4aaa@abv.bg>
2026-07-10 18:44:37 +0300

    Create requester.js

---
+++ src/api/requester.js
@@ -0,0 +1,54 @@
+import { getAccessToken } from "../utils/authUtils";
+
+
+async function requester(method, ur, data) {
+    const options = {};
+
+    const accessToken = getAccessToken();
+
+    if (accessToken) {
+        options.headers = {
+            ...options.headers,
+            "X-Authorization": accessToken,
+        };
+    }
+
+    if (method !== "GET") {
+        options.method = method;
+    }
+
+    if (data) {
+        options.headers = {
+            ...options.headers,
+            "Content-Type": "application/json",
+        };
+        options.body = JSON.stringify(data);
+    }
+
+    const response = await fetch(ur, options);
+
+    if (response.status === 204) {
+        return;
+    }
+
+    const result = await response.json();
+
+    if (!response.ok) {
+        console.log(result);
+        throw result;
+    }
+
+    return result;
+}
+
+export const get = requester.bind(null, "GET");
+export const post = requester.bind(null, "POST");
+export const put = requester.bind(null, "PUT");
+export const del = requester.bind(null, "DELETE");
+
+export default {
+    get,
+    post,
+    put,
+    del,
+};

79fc3291baf199eae275a55bcc04e5d65500e13a
Mariyan Donchev <pi4aaa@abv.bg>
2026-07-11 21:08:46 +0300

    Update products-api.js

--- src/api/products-api.js
+++ src/api/products-api.js
@@ -2,36 +2,36 @@ import * as request from "./requester"
 
 const BASE_URL = 'http://localhost:3030/data';
 
-export const getAll = async (product, sortProducts) => {
-    try {
-        let urlSearchParams = ''
+// export const getAll = async (product, sortProducts) => {
+//     try {
+//         let urlSearchParams = ''
 
-        if (sortProducts === 'price-asc') {
-            urlSearchParams = new URLSearchParams({
-                sortBy: 'price',
-            })
-        } else if (sortProducts === 'price-desc') {
-            urlSearchParams = new URLSearchParams({
-                sortBy: 'price desc',
-            })
-        } else if (sortProducts === 'name-desc') {
-            urlSearchParams = new URLSearchParams({
-                sortBy: 'name desc',
-            })
-        } else {
-            urlSearchParams = new URLSearchParams({
-                sortBy: 'name',
-            })
-        }
+//         if (sortProducts === 'price-asc') {
+//             urlSearchParams = new URLSearchParams({
+//                 sortBy: 'price',
+//             })
+//         } else if (sortProducts === 'price-desc') {
+//             urlSearchParams = new URLSearchParams({
+//                 sortBy: 'price desc',
+//             })
+//         } else if (sortProducts === 'name-desc') {
+//             urlSearchParams = new URLSearchParams({
+//                 sortBy: 'name desc',
+//             })
+//         } else {
+//             urlSearchParams = new URLSearchParams({
+//                 sortBy: 'name',
+//             })
+//         }
 
-        const result = await request.get(`${BASE_URL}/${product}?${urlSearchParams.toString()}`);
-        const products = Object.values(result);
-        return products;
-    } catch (err) {
-        console.log(err.message);
-        throw err
-    }
-}
+//         const result = await request.get(`${BASE_URL}/${product}?${urlSearchParams.toString()}`);
+//         const products = Object.values(result);
+//         return products;
+//     } catch (err) {
+//         console.log(err.message);
+//         throw err
+//     }
+// }
 
 export const getOne = (product, productId) => request.get(`${BASE_URL}/${product}/${productId}`);
 
@@ -40,7 +40,7 @@ export const update = (product, productId, data) => request.put(`${BASE_URL}/${p
 export const remove = (product, productId) => request.del(`${BASE_URL}/${product}/${productId}`);
 
 const productsAPI = {
-    getAll,
+    // getAll,
     getOne,
     update,
     remove,

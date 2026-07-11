32587a1a97fa56b3f0d50c1b32710669dd3ef7ac
Mariyan Donchev <pi4aaa@abv.bg>
2026-07-11 17:47:11 +0300

    Update useProductsSupa.js

--- src/hooks/useProductsSupa.js
+++ src/hooks/useProductsSupa.js
@@ -2,76 +2,86 @@ import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase.js";
 
 export function useAllProducts(type, sortProducts) {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        async function loadProducts() {
-            try {
-            console.log(sortProducts)
-                if (sortProducts === 'price-asc') {
-                    const { data, error } = await supabase
-                        .from(type)
-                        .select("*")
-                        .order("price", { ascending: true });
-                        setProducts(data);
+  useEffect(() => {
+    async function loadProducts() {
+      try {
+        if (sortProducts === "price-asc") {
+          const { data, error } = await supabase
+            .from(type)
+            .select("*")
+            .order("price", { ascending: true });
+          setProducts(data);
 
-                } else if (sortProducts === 'price-desc') {
-                    const { data, error } = await supabase
-                        .from(type)
-                        .select("*")
-                        .order("price", { ascending: false });
-                        setProducts(data);
+          if (error) {
+            console.error(error);
+            return;
+          }
+        } else if (sortProducts === "price-desc") {
+          const { data, error } = await supabase
+            .from(type)
+            .select("*")
+            .order("price", { ascending: false });
+          setProducts(data);
 
-                } else if (sortProducts === 'name-desc') {
-                    const { data, error } = await supabase
-                        .from(type)
-                        .select("*")
-                        .order("name", { ascending: false });
-                        setProducts(data);
+          if (error) {
+            console.error(error);
+            return;
+          }
+        } else if (sortProducts === "name-desc") {
+          const { data, error } = await supabase
+            .from(type)
+            .select("*")
+            .order("name", { ascending: false });
+          setProducts(data);
 
-                } else {
-                    const { data, error } = await supabase
-                        .from(type)
-                        .select("*")
-                        .order("name", { ascending: true });
-                        setProducts(data);
-                }
+          if (error) {
+            console.error(error);
+            return;
+          }
+        } else {
+          const { data, error } = await supabase
+            .from(type)
+            .select("*")
+            .order("name", { ascending: true });
+          setProducts(data);
 
-                if (error) {
-                    console.error(error);
-                    return;
-                }
-               
-            } catch (error) {
-                console.error(error);
-            }
+          if (error) {
+            console.error(error);
+            return;
+          }
         }
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
-        loadProducts();
-    }, [type, sortProducts]);
-    return [products]
+    loadProducts();
+  }, [type, sortProducts]);
+  return [products];
 }
 
 export function oneProduct(type, productId) {
-    const [product, setProduct] = useState([]);
+  const [product, setProduct] = useState([]);
 
-    useEffect(() => {
-        async function loadOneProducts() {
-            const { data, error } = await supabase
-                .from(type)
-                .select("*")
-                .eq("_id", productId)
-                .single();
+  useEffect(() => {
+    async function loadOneProducts() {
+      const { data, error } = await supabase
+        .from(type)
+        .select("*")
+        .eq("_id", productId)
+        .single();
 
-            if (error) {
-                console.error(error);
-                return;
-            }
+      if (error) {
+        console.error(error);
+        return;
+      }
 
-            setProduct(data);
-        }
+      setProduct(data);
+    }
 
-        loadOneProducts();
-    }, [type, productId]);
-    return [product]
+    loadOneProducts();
+  }, [type, productId]);
+  return [product];
 }

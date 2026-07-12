8497ade38bae31c8bc8559f446ebeced9884e287
Mariyan Donchev <pi4aaa@abv.bg>
2026-07-12 20:50:42 +0300

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,42 +1,47 @@
-
-import { login, register, logout } from "../api/auth-api"
-import { useAuthContext } from "../contexts/AuthContext"
+import { login, register, logout } from "../api/auth-api";
+import { useAuthContext } from "../contexts/AuthContext";
+import { supabase } from "../lib/supabase";
 
 export const useLogin = () => {
+  const { changeAutState } = useAuthContext();
 
-    const { changeAutState } = useAuthContext();
+  const loginHandler = async (email, password) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    changeAutState(data);
 
-    const loginHandler = async (email, password, fullName) => {
-        const { password: _, ...authData } = await login(email, password, fullName);
-        changeAutState(authData);
+    return data;
+  };
 
-        return authData
-    };
-
-    return loginHandler
-}
+  return loginHandler;
+};
 
 export const useRegister = () => {
-    const { changeAutState } = useAuthContext();
+  const { changeAutState } = useAuthContext();
 
-    const registerHandler = async (email, password, fullName) => {
-        const { password: _, ...authData } = await register(email, password, fullName);
-        changeAutState(authData);
+  const registerHandler = async (email, password) => {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
 
-        return authData
-    };
+    changeAutState(data);
 
-    return registerHandler;
-}
+    return data;
+  };
+
+  return registerHandler;
+};
 
 export const useLogout = () => {
-    const { logout: localLogout } = useAuthContext();
+  const { logout: localLogout } = useAuthContext();
 
-    const logoutHandler = async () => {
-        localLogout();
-        await logout();
-    };
+  const logoutHandler = async () => {
+    const { error } = await supabase.auth.signOut();
+    localLogout();
+  };
 
-    return logoutHandler;
-}
-
+  return logoutHandler;
+};
